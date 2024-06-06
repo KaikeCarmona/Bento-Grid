d7db8bae384688370b120ae53c33f239d4283862
Kaike Carmona <kkaike371@gmail.com>
2024-06-06 01:32:58 -0300

    só falta a terceira tela que o vitor esta fazendo

--- bento design/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function() {
-    var header = document.getElementById('#teste');
-    console.log('Scroll event detected');
-    if (window.scrollY > 0) {
-      header.classList.add('scrolled');
-    } else {
-      header.classList.remove('scrolled');
-    }
-  });
